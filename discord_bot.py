8550da3d8e4a6f5251d861d582bee70582913032
Ramil Mavlyutov <63664630+stirk1337@users.noreply.github.com>
2022-04-24 04:04:13 +0500

    Update discord_bot.py

--- discord_bot.py
+++ discord_bot.py
@@ -1,3 +1,5 @@
+import asyncio
+from table2ascii import table2ascii as t2a, PresetStyle
 import discord
 from discord.ext import commands
 from discord.utils import get
@@ -7,53 +9,47 @@ import youtube_dl
 from requests import get
 from youtube_search import YoutubeSearch
 
-token = ""
-channel_id = "
+print('Я запустился')
 
-bot = commands.Bot(command_prefix="!", intents = discord.Intents.all())
+bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
+
+queue = []
+is_skip = False
+
+token = ""
+channel_id = 0
+emojis = []  # your emoji list, you need to has plus15 and minus15 emojis, for social credit function work!
+
+bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
+ffmpeg_options = {
+    'options': '-vn',
+    "before_options": "-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5"
+}
+YDL_OPTIONS = {'format': "bestaudio", 'noplaylist': 'True'}
+
 
 @commands.cooldown(1, 3, commands.BucketType.user)
-
 @bot.command()
 async def mycredit(ctx):
     user = ctx.message.author.id
     with open("info.json") as f:
-            file = json.load(f)
+        file = json.load(f)
     credit = 0
-    if(str(user) in file['users']):
+    if str(user) in file['users']:
         credit = file['users'][str(user)]
-    
-    #your emoji list, you need to has plus15 and minus15 emojis, for social credit function work!
-    emojis = []
-
-
     emoji = discord.utils.get(bot.emojis, name=random.choice(emojis))
 
     channel = bot.get_channel(channel_id)
     await channel.send("<@" + str(user) + ">" + ", у Вас " + str(credit) + " Social Credit " + str(emoji))
 
-@bot.command()
-async def join(ctx):
-    if ctx.author.voice is None:
-        await ctx.send("Вы не в голосовом канале!")
-    voice_channel = ctx.author.voice.channel
-    if ctx.voice_client is None:
-        await voice_channel.connect()
-    else:
-        await ctx.voice_client.move_to(voice_channel)
 
 @bot.command()
 async def disconnect(ctx):
     await ctx.voice_client.disconnect()
 
-@bot.command()
-async def stop(ctx):
-    vc = ctx.voice_client
-    vc.stop()
 
 @bot.command()
 async def play(ctx, *urls):
-
     if ctx.author.voice is None:
         await ctx.send("Вы не в голосовом канале!")
     voice_channel = ctx.author.voice.channel
@@ -62,33 +58,99 @@ async def play(ctx, *urls):
     else:
         await ctx.voice_client.move_to(voice_channel)
 
-
     url = ' '.join(urls)
-    ctx.voice_client.stop()
-    ffmpeg_options = {
-    'options': '-vn',
-    "before_options": "-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5"
-}
-    YDL_OPTIONS = {'format':"bestaudio", 'noplaylist':'True'}
+    if len(url) == 0:
+        return
+
     vc = ctx.voice_client
 
     with youtube_dl.YoutubeDL(YDL_OPTIONS) as ydl:
         try:
             get(url)
         except:
-            results = YoutubeSearch(url, max_results=1).to_dict()
+            results = YoutubeSearch(url, max_results=1)
+            results = results.to_dict()
             url = "https://www.youtube.com" + results[0]['url_suffix']
-            info = ydl.extract_info(url, download = False)
+            info = ydl.extract_info(url, download=False)
             url2 = info['formats'][0]['url']
-            source = await discord.FFmpegOpusAudio.from_probe(url2, **ffmpeg_options)
-            vc.play(source)
+            duration = info['duration']
+            title = info['title']
+            author = ctx.author
+            queue.append([url, url2, duration, title, author])
             channel = bot.get_channel(channel_id)
-            await channel.send(url)
+            print(author)
+            await channel.send("Добавлено в очередь: " + title)
+            if not playingaudio(ctx):
+                q = queue.pop(0)
+                await true_play(q[0], q[1], q[2], q[3], q[4], vc)
+
         else:
-            info = ydl.extract_info(url, download = False)
+            info = ydl.extract_info(url, download=False)
             url2 = info['formats'][0]['url']
-            source = await discord.FFmpegOpusAudio.from_probe(url2, **ffmpeg_options)
-            vc.play(source)
+            duration = info['duration']
+            title = info['title']
+            author = ctx.author
+            queue.append([url, url2, duration, title, author])
+            channel = bot.get_channel(channel_id)
+            await channel.send("Добавлено в очередь: " + title)
+            if not playingaudio(ctx):
+                q = queue.pop(0)
+                await true_play(q[0], q[1], q[2], q[3], q[4], vc)
+                await slip(duration, vc)
+
+async def slip(duration, vc):
+    global is_skip
+    await asyncio.sleep(duration)
+    if not is_skip:
+        q = queue.pop(0)
+        is_skip = False
+        await true_play(q[0], q[1], q[2], q[3], q[4], vc)
+
+@bot.command()
+async def skip(ctx):
+    global is_skip
+    is_skip = True
+    vc = ctx.voice_client
+    vc.stop()
+    q = queue.pop(0)
+    await true_play(q[0], q[1], q[2], q[3], q[4], vc)
+
+@bot.command()
+async def list(ctx):
+    table = []
+    i = 1
+    for q in queue:
+        time = str(q[2]//60) + ":" + str(q[2]%60)
+        table.append([i, q[3], time, q[4]])
+        i+=1
+    output = t2a(
+        header=["Номер", "Название", "Продолжительность", "Поставил"],
+        body=table,
+        style=PresetStyle.thin_compact
+    )
+    channel = bot.get_channel(channel_id)
+    await channel.send(f"```\n{output}\n```")
+
+
+@bot.command()
+async def remove(ctx, number):
+    q = queue.pop(int(number)-1)
+    channel = bot.get_channel(channel_id)
+    await channel.send("Удалено из очереди: " + str(q[3]))
+
+async def true_play(url, url2, duration, title, author,  vc):
+    source = await discord.FFmpegOpusAudio.from_probe(url2, **ffmpeg_options)
+    channel = bot.get_channel(channel_id)
+    vc.play(source)
+    await channel.send("Сейчас играет: " + title + "\n"+url)
+
+
+
+def playingaudio(ctx):
+    guild = ctx.guild
+    voice_client: discord.VoiceClient = discord.utils.get(bot.voice_clients, guild=guild)
+    return voice_client.is_playing()
+
 
 @bot.event
 async def on_raw_reaction_add(payload):
@@ -112,7 +174,6 @@ async def on_raw_reaction_add(payload):
             with open('info.json', 'w') as f:
                 f.write(json.dumps(file))
 
-
             channel = bot.get_channel(channel_id)
             emoji = discord.utils.get(bot.emojis, name='plus15')
 
@@ -133,5 +194,6 @@ async def on_raw_reaction_add(payload):
 
             emoji = discord.utils.get(bot.emojis, name='minus15')
             await channel.send("<@" + str(message.author.id) + ">" + " получил -15 Social Credit! " + str(emoji))
+
 
 bot.run(token)
